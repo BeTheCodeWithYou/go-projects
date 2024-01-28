518bbbc1aed45784d30732e550428804ee1f542f
Neeraj Sidhaye <bethecodewithyou@gmail.com>
2024-01-28 20:52:31 +0000

    htmx and go - morse code decoder

---
+++ morse-code/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+	//"io"
+	"net/http"
+	"strings"
+	"text/template"
+)
+
+var morsemap = map[string]string{
+	".-":     "A",
+	"-...":   "B",
+	"-.-.":   "C",
+	"-..":    "D",
+	".":      "E",
+	"..-.":   "F",
+	"--.":    "G",
+	"....":   "H",
+	"..":     "I",
+	".---":   "J",
+	"-.-":    "K",
+	".-..":   "L",
+	"--":     "M",
+	"-.":     "N",
+	"---":    "O",
+	".--.":   "P",
+	"--.-":   "Q",
+	".-.":    "R",
+	"...":    "S",
+	"-":      "T",
+	"..-":    "U",
+	"...-":   "V",
+	".--":    "W",
+	"-..-":   "X",
+	"-.--":   "Y",
+	"--..":   "Z",
+	".----":  "1",
+	"..---":  "2",
+	"...--":  "3",
+	"....-":  "4",
+	".....":  "5",
+	"-....":  "6",
+	"--...":  "7",
+	"---..":  "8",
+	"----.":  "9",
+	"-----":  "0",
+	".-.-.-": ".",
+	"--..--": ",",
+	"..--..": "?",
+	"-.-.--": "!",
+	"-....-": "-",
+	"-..-.":  "/",
+	".--.-.": "@",
+	"-.--.":  "(",
+	"-.--.-": ")",
+}
+
+var morseValMap = map[string]string{
+	"A": ".-",
+	"B": "-...",
+	"C": "-.-.",
+	"D": "-..",
+	"E": ".",
+	"F": "..-.",
+	"G": "--.",
+	"H": "....",
+	"I": "..",
+	"J": ".---",
+	"K": "-.-",
+	"L": ".-..",
+	"M": "--",
+	"N": "-.",
+	"O": "---",
+	"P": ".--.",
+	"Q": "--.-",
+	"R": ".-.",
+	"S": "...",
+	"T": "-",
+	"U": "..-",
+	"V": "...-",
+	"W": ".--",
+	"X": "-..-",
+	"Y": "-.--",
+	"Z": "--..",
+	"1": ".----",
+	"2": "..---",
+	"3": "...--",
+	"4": "....-",
+	"5": ".....",
+	"6": "-....",
+	"7": "--...",
+	"8": "---..",
+	"9": "----.",
+	"0": "-----",
+	".": ".-.-.-",
+	",": "--..--",
+	"?": "..--..",
+	"!": "-.-.--",
+	"-": "-....-",
+	"/": "-..-.",
+	"@": ".--.-.",
+	"(": "-.--.",
+	")": "-.--.-",
+}
+
+func main() {
+
+	str := DecodeMorse(".... . -.--   .--- ..- -.. .")
+	fmt.Println(str)
+
+	http.HandleFunc("/", pageLoadHandler)
+	http.HandleFunc("/morsecode", morseCodeHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func pageLoadHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl.Execute(w, nil)
+}
+
+func morseCodeHandler(w http.ResponseWriter, r *http.Request) {
+	htmlStr := fmt.Sprintf("<p>%s</p>", DecodeMorse(r.PostFormValue("morse-code")))
+	t, _ := template.New("t").Parse(htmlStr)
+	t.Execute(w, nil)
+}
+
+func DecodeMorse(morseCode string) string {
+	morseCode = strings.TrimSpace(morseCode)
+	wrds := strings.Split(morseCode, "   ")
+	for i, word := range wrds {
+		chrs := strings.Split(word, " ")
+		for i, chr := range chrs {
+			chrs[i] = morsemap[chr]
+		}
+		letter := strings.Join(chrs, "")
+		wrds[i] = letter
+	}
+
+	return strings.Join(wrds, " ")
+}
